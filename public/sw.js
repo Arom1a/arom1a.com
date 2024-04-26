29e4e7a04ea44511082fe1ea568be02735ffdee6
Aromia <f2940015504@gmail.com>
2024-04-26 22:41:27 +0800

    chore: add log

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,12 @@
 const addResourcesToCache = async (resources) => {
   const cache = await caches.open("bookmark-cache-v1");
+  console.log("Opened cache");
   await cache.addAll(resources);
 };
 
 self.addEventListener("install", (event) => {
   event.waitUntil(addResourcesToCache(["/bookmarks"]));
+  console.log("Service Worker finished installing");
 });
 
 const cacheFirst = async (request) => {
